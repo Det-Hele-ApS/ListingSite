109d2f1f1a5904efdf85fd0661400c6f33972007
Illia Mikhalkou <ilya1992m@gmail.com>
2018-06-14 03:01:14 +0300

    Theme added to the listing page

--- ListingApp/ListingApp.WebApp/Controllers/ListingController.cs
+++ ListingApp/ListingApp.WebApp/Controllers/ListingController.cs
@@ -13,17 +13,21 @@ namespace ListingApp.WebApp.Controllers
 		private readonly IServiceService serviceService;
 
 		private readonly IEscortService escortService;
-		
+
+		private readonly IRegionService regionService;
+
 		private readonly ICityService cityService;
 
 		public ListingController(IEscortTypeService escortTypeService,
 			IServiceService serviceService,
 			IEscortService escortService,
+			IRegionService regionService,
 			ICityService cityService)
 		{
 			this.escortTypeService = escortTypeService;
 			this.serviceService = serviceService;
 			this.escortService = escortService;
+			this.regionService = regionService;
 			this.cityService = cityService;
 		}
 
@@ -31,6 +35,7 @@ namespace ListingApp.WebApp.Controllers
 		{
 			this.ViewBag.Types = await this.escortTypeService.GetAll();
 			this.ViewBag.Services = await this.serviceService.GetAll();
+			this.ViewBag.Regions = await this.regionService.GetAll();
 			this.ViewBag.Cities = await this.cityService.GetAll();
 
 			this.ViewBag.Type = await this.GetRouteParam(first, second, third, RouteParams.Type);
